4c3bee709e5b7a27712bfea02761ffd521518193
rafaover <rafamsilva@gmail.com>
2023-08-18 14:03:57 +1000

    Renamed model from recipe to Dish.kt

--- app/src/main/java/com/feeltheboard/eatas/model/Dish.kt
+++ app/src/main/java/com/feeltheboard/eatas/model/Dish.kt
@@ -3,8 +3,8 @@ package com.feeltheboard.eatas.model
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
-data class Recipe(
-    @StringRes val dayRes: Int,
+data class Dish(
+    val dayRes: Int,
     @StringRes val titleRes: Int,
     @DrawableRes val imageRes: Int,
     @StringRes val descriptionRes: Int
