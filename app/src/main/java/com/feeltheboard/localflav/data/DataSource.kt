918a0cedc9f8625db90d322964d7ea4384b0acaf
rafaover <rafamsilva@gmail.com>
2023-08-21 14:09:45 +1000

    Created dishes listOf datasource - renamed all images for proper sync with other model fields.

---
+++ app/src/main/java/com/feeltheboard/localflav/data/DataSource.kt
@@ -0,0 +1,38 @@
+package com.feeltheboard.localflav.data
+
+import com.feeltheboard.eatas.R
+
+object DataSource {
+    val dishes = listOf(
+        Dish(R.string.day_1, R.string.title_1, R.drawable.image_1, R.string.description_1),
+        Dish(R.string.day_2, R.string.title_2, R.drawable.image_2, R.string.description_2),
+        Dish(R.string.day_3, R.string.title_3, R.drawable.image_3, R.string.description_3),
+        Dish(R.string.day_4, R.string.title_4, R.drawable.image_4, R.string.description_4),
+        Dish(R.string.day_5, R.string.title_5, R.drawable.image_5, R.string.description_5),
+        Dish(R.string.day_6, R.string.title_6, R.drawable.image_6, R.string.description_6),
+        Dish(R.string.day_7, R.string.title_7, R.drawable.image_7, R.string.description_7),
+        Dish(R.string.day_8, R.string.title_8, R.drawable.image_8, R.string.description_8),
+        Dish(R.string.day_9, R.string.title_9, R.drawable.image_9, R.string.description_9),
+        Dish(R.string.day_10, R.string.title_10, R.drawable.image_10, R.string.description_10),
+        Dish(R.string.day_11, R.string.title_11, R.drawable.image_11, R.string.description_11),
+        Dish(R.string.day_12, R.string.title_12, R.drawable.image_12, R.string.description_12),
+        Dish(R.string.day_13, R.string.title_13, R.drawable.image_13, R.string.description_13),
+        Dish(R.string.day_14, R.string.title_14, R.drawable.image_14, R.string.description_14),
+        Dish(R.string.day_15, R.string.title_15, R.drawable.image_15, R.string.description_15),
+        Dish(R.string.day_16, R.string.title_16, R.drawable.image_16, R.string.description_16),
+        Dish(R.string.day_17, R.string.title_17, R.drawable.image_17, R.string.description_17),
+        Dish(R.string.day_18, R.string.title_18, R.drawable.image_18, R.string.description_18),
+        Dish(R.string.day_19, R.string.title_19, R.drawable.image_19, R.string.description_19),
+        Dish(R.string.day_20, R.string.title_20, R.drawable.image_20, R.string.description_20),
+        Dish(R.string.day_21, R.string.title_21, R.drawable.image_21, R.string.description_21),
+        Dish(R.string.day_22, R.string.title_22, R.drawable.image_22, R.string.description_22),
+        Dish(R.string.day_23, R.string.title_23, R.drawable.image_23, R.string.description_23),
+        Dish(R.string.day_24, R.string.title_24, R.drawable.image_24, R.string.description_24),
+        Dish(R.string.day_25, R.string.title_25, R.drawable.image_25, R.string.description_25),
+        Dish(R.string.day_26, R.string.title_26, R.drawable.image_26, R.string.description_26),
+        Dish(R.string.day_27, R.string.title_27, R.drawable.image_27, R.string.description_27),
+        Dish(R.string.day_28, R.string.title_28, R.drawable.image_28, R.string.description_28),
+        Dish(R.string.day_29, R.string.title_29, R.drawable.image_29, R.string.description_29),
+        Dish(R.string.day_30, R.string.title_30, R.drawable.image_30, R.string.description_30)
+    )
+}
