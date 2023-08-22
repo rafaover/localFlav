711fda6a36da31005eb9fad56d3b1deb00159d55
rafaover <rafamsilva@gmail.com>
2023-08-22 10:36:53 +1000

    DishCard - Added borderstroke and dimension to dimens.xml

--- app/src/main/java/com/feeltheboard/localflav/components/DishCard.kt
+++ app/src/main/java/com/feeltheboard/localflav/components/DishCard.kt
@@ -1,14 +1,18 @@
 package com.feeltheboard.localflav.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.feeltheboard.localflav.R
 import com.feeltheboard.localflav.data.Dish
 import com.feeltheboard.localflav.ui.theme.LocalFlavTheme
@@ -21,7 +25,11 @@ fun DishCard(
     Card(
         modifier = Modifier.fillMaxWidth(),
         elevation = CardDefaults.cardElevation(dimensionResource(R.dimen.card_elevation)),
-        shape = CardDefaults.shape
+        shape = CardDefaults.shape,
+        border = BorderStroke(
+            dimensionResource(R.dimen.card_border),
+            MaterialTheme.colorScheme.secondary
+        )
     ) {
         Row {
             DishDayTitleCard(
