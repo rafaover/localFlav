73803d4ba11e5cf856c56ed505b7a90dce81ce7a
rafaover <rafamsilva@gmail.com>
2023-08-22 10:26:18 +1000

    DishCard.kt, complementary composables. - Added elevation and shape to card.

--- app/src/main/java/com/feeltheboard/localflav/components/DishCard.kt
+++ app/src/main/java/com/feeltheboard/localflav/components/DishCard.kt
@@ -1,10 +1,13 @@
 package com.feeltheboard.localflav.components
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.feeltheboard.localflav.R
 import com.feeltheboard.localflav.data.Dish
@@ -16,12 +19,28 @@ fun DishCard(
     modifier: Modifier = Modifier
 ) {
     Card(
-        modifier.fillMaxWidth()
+        modifier = Modifier.fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(dimensionResource(R.dimen.card_elevation)),
+        shape = CardDefaults.shape
     ) {
-        Column {
-            DishDayTitleCard(day = dish.dayRes, title = dish.titleRes)
-            DishImageCard(image = dish.imageRes, caption = dish.titleRes)
-            DishDescriptionCard(description = dish.descriptionRes)
+        Row {
+            DishDayTitleCard(
+                day = dish.dayRes,
+                title = dish.titleRes
+            )
+        }
+        Row {
+            Column {
+                DishImageCard(
+                    image = dish.imageRes,
+                    caption = dish.titleRes
+                )
+            }
+            Column {
+                DishDescriptionCard(
+                    description = dish.descriptionRes
+                )
+            }
         }
     }
 }
@@ -33,9 +52,9 @@ fun DishCardPreview() {
         DishCard(
             dish = Dish(
                 R.string.day_1,
-                R.string.title_1,
-                R.drawable.image_1,
-                R.string.description_1
+                R.string.title_17,
+                R.drawable.image_17,
+                R.string.description_17
             )
         )
     }
