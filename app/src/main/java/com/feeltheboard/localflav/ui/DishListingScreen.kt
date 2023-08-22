0a98e9702f3a915ae9a9b8f20e508ad27a6fc090
rafaover <rafamsilva@gmail.com>
2023-08-22 13:06:21 +1000

    DishListingScreen.kt: LazyColumn update. - Added verticalArrangement = spacedBy cards - modifier for padding

--- app/src/main/java/com/feeltheboard/localflav/ui/DishListingScreen.kt
+++ app/src/main/java/com/feeltheboard/localflav/ui/DishListingScreen.kt
@@ -1,11 +1,16 @@
 package com.feeltheboard.localflav.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.feeltheboard.localflav.R
 import com.feeltheboard.localflav.components.DishCard
 import com.feeltheboard.localflav.components.LocalFlavTopAppBar
 import com.feeltheboard.localflav.data.DataSource.dishes
@@ -17,7 +22,11 @@ fun DishListingScreen() {
     Scaffold(
         topBar = { LocalFlavTopAppBar() },
         content = {
-            LazyColumn(contentPadding = it) {
+            LazyColumn(
+                contentPadding = it,
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)),
+                modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium))
+            ) {
                 items(dishes) {
                     DishCard(
                         dish = it
