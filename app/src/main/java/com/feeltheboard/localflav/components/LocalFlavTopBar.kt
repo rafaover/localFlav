8c3aede9d95ba17e9501d8e2a06de2508ce88541
rafaover <rafamsilva@gmail.com>
2023-08-23 12:06:51 +1000

    LocalFlavTopBar.kt: changed icon and added content for navigationIcon at topbar.

--- app/src/main/java/com/feeltheboard/localflav/components/LocalFlavTopBar.kt
+++ app/src/main/java/com/feeltheboard/localflav/components/LocalFlavTopBar.kt
@@ -5,7 +5,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,7 +16,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.feeltheboard.localflav.R
+import com.feeltheboard.localflav.ui.theme.LocalFlavTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -28,7 +32,7 @@ fun LocalFlavTopAppBar(modifier: Modifier = Modifier) {
                 Image(
                     modifier = Modifier
                         .padding(dimensionResource(id = R.dimen.padding_small)),
-                    painter = painterResource(R.drawable.menu_access),
+                    painter = painterResource(R.drawable.food_bank_24),
                     contentDescription = stringResource(R.string.app_name)
                 )
                 Text(
@@ -39,11 +43,19 @@ fun LocalFlavTopAppBar(modifier: Modifier = Modifier) {
         },
         navigationIcon = {
             IconButton(
+                content = { Icon(painter = painterResource(R.drawable.menu_access), contentDescription = "List of Dishes") },
                 onClick = { /*TODO*/ },
                 enabled = false,
-            ) {
-
-            }
+                colors = IconButtonDefaults.filledIconButtonColors()
+            )
         }
     )
+}
+
+@Preview
+@Composable
+fun LocalFlavTopBarPreview() {
+    LocalFlavTheme {
+        LocalFlavTopAppBar()
+    }
 }
