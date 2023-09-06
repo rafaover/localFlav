8351fda38258315a0156cd3ee5f62fe1b95e95e2
rafaover <rafamsilva@gmail.com>
2023-09-06 18:19:39 +1000

    Created viewmodel and val to save state of checkbox on card.

--- app/src/main/java/com/feeltheboard/localflav/vm/DishViewModel.kt
+++ app/src/main/java/com/feeltheboard/localflav/vm/DishViewModel.kt
@@ -1,14 +1,8 @@
 package com.feeltheboard.localflav.vm
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 
 class DishViewModel: ViewModel() {
-    private val _cardCheckBoxState = MutableStateFlow(false)
-    val cardCheckBoxState: StateFlow<Boolean> = _cardCheckBoxState
-
-    fun setCheckBoxState(isChecked: Boolean) {
-        _cardCheckBoxState.value = isChecked
-    }
+    val cardCheckBoxIsChecked = mutableStateOf(false)
 }
