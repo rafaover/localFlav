fc884b936a037725c485af65c027fdbce8d730d6
rafaover <rafamsilva@gmail.com>
2023-09-11 15:27:25 +1000

    Edited Strings for Dialog - Added new text and strings to res/xml

--- app/src/main/java/com/feeltheboard/localflav/ui/components/MainAlertDialog.kt
+++ app/src/main/java/com/feeltheboard/localflav/ui/components/MainAlertDialog.kt
@@ -1,14 +1,22 @@
 package com.feeltheboard.localflav.ui.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.feeltheboard.localflav.R
 import com.feeltheboard.localflav.vm.DishViewModel
 
 @Composable
@@ -19,15 +27,21 @@ fun MainAlertDialog(viewModel: DishViewModel = DishViewModel()) {
             onDismissRequest = {
                 showDialog = false
             },
-            icon = { Icon(Icons.Filled.Info, contentDescription = "localFlav") },
-            title = {
-                Text(text = "Title")
+            icon = {
+                Icon(
+                    imageVector = Icons.Filled.Info,
+                    contentDescription = stringResource(R.string.app_name)
+                )
             },
             text = {
-                Text(
-                    "This area typically contains the supportive text " +
-                            "which presents the details regarding the Dialog's purpose."
-                )
+                Column {
+                    Text(stringResource(R.string.about_text))
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(
+                        text = stringResource(R.string.email),
+                        style = MaterialTheme.typography.labelLarge
+                    )
+                }
             },
             confirmButton = {
                 TextButton(
@@ -35,7 +49,7 @@ fun MainAlertDialog(viewModel: DishViewModel = DishViewModel()) {
                         showDialog = false
                     }
                 ) {
-                    Text("Thanks")
+                    Text(stringResource(R.string.close))
                 }
             },
         )
